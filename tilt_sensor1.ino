1a96e87179914c4648d9220e15453cfe4fc316ec
Mohamed Magdi <138686935+mohamedmagdi11@users.noreply.github.com>
2023-07-31 17:31:24 +0300

    Add files via upload

---
+++ tilt_sensor1.ino
@@ -0,0 +1,38 @@
+// C++ code
+//
+
+int sw = A0 ;
+int motor = 11 ;
+
+int speed =0 ;
+int swValue = 0;
+
+void setup()
+{
+  pinMode(sw,INPUT);
+  pinMode(motor,OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+  
+  swValue= analogRead(sw);
+  
+   speed =map(swValue,0,1023,0,255);
+  
+    if(swValue <0 || swValue >255)
+  {
+    analogWrite(motor,LOW);
+      
+  }
+  
+  else
+  {
+    
+      analogWrite(motor,-speed);
+  }
+  
+}
